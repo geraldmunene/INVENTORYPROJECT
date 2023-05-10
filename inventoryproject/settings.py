b9fa6ea1c203baae10072face1720b9d1e17dfc5
geraldmunene <124464681+geraldmunene@users.noreply.github.com>
2023-05-10 14:59:30 +0300

    Update settings.py

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -143,7 +143,7 @@ LOGIN_URL = "user-login"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 
-EMAIL_PORT = 587
+EMAIL_PORT = 
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 
 EMAIL_HOST_PASSWORD =
